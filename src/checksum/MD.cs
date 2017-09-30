4316b8ac90c8fda5719ab07d39141b3a0fb944a2
Kai Borchert <kai.borchert@outlook.com>
2017-09-30 14:59:21 +0200

    checksum

---
+++ src/checksum/MD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace checksum
+{
+    public class MD
+    {
+        public static string MD5(string path)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                using (BufferedStream bs = new BufferedStream(fs))
+                using (var md5 = System.Security.Cryptography.MD5.Create())
+                {
+                    var comp = md5.ComputeHash(bs);
+                    var hash = new StringBuilder();
+                    foreach (byte b in comp)
+                    {
+                        hash.Append(b.ToString("X2"));
+                    }
+                    return hash.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
---
+++ src/checksum/SHA.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace checksum
+{
+    public class SHA
+    {
+        public static string SHA1(string path)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                using (BufferedStream bs = new BufferedStream(fs))
+                using (SHA1Managed sha = new SHA1Managed())
+                {
+                    var comp = sha.ComputeHash(bs);
+                    var hash = new StringBuilder(comp.Length * 2);
+
+                    foreach (byte b in comp)
+                    {
+                        hash.Append(b.ToString("X2"));
+                    }
+                    return hash.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+        public static string SHA256(string path)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                using (BufferedStream bs = new BufferedStream(fs))
+                using (SHA256Managed sha = new SHA256Managed())
+                {
+                    var comp = sha.ComputeHash(bs);
+                    var hash = new StringBuilder(comp.Length * 2);
+
+                    foreach (byte b in comp)
+                    {
+                        hash.Append(b.ToString("X2"));
+                    }
+                    return hash.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+        public static string SHA512(string path)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.Open))
+                using (BufferedStream bs = new BufferedStream(fs))
+                using (SHA512Managed sha = new SHA512Managed())
+                {
+                    var comp = sha.ComputeHash(bs);
+                    var hash = new StringBuilder(comp.Length * 2);
+
+                    foreach (byte b in comp)
+                    {
+                        hash.Append(b.ToString("X2"));
+                    }
+                    return hash.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+    }
+}
